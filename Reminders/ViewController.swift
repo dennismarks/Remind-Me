29661f342f51babd7fa4966f4660f0e593f1357f
dennismarks <dennismarkshome@gmail.com>
2019-05-26 22:25:22 -0400

    add tab bar add new category implementation; extension for UIcolor to read hex values

--- Reminders/ViewController.swift
+++ Reminders/ViewController.swift
@@ -37,6 +37,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return array.count
     }
     
+    @objc func addButtonPressed() {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new category", message: .none, preferredStyle: .alert)
+        let add = UIAlertAction(title: "Add Category", style: .default) { (UIAlertAction) in
+            if let text = textField.text {
+                let item = Category(context: self.context)
+                item.title = text
+                item.done = false
+                self.array.append(item)
+                self.save()
+                self.tableView.reloadData()
+                print("Saved")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (UIAlertAction) in }
+        alert.addTextField { (UITextField) in
+            UITextField.placeholder = "Create new item"
+            textField = UITextField
+        }
+        alert.addAction(add)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
             self.context.delete(self.array[indexPath.row])
@@ -70,6 +94,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             print("Error fetchin data from context \(error)")
         }
     }
-    
 
 }
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
