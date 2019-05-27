c2398dda35e9799378e66859002a6efe94ca02f5
dennismarks <dennismarkshome@gmail.com>
2019-05-26 22:17:41 -0400

    table view delegate methods

---
+++ Reminders/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Dennis M on 2019-05-25.
+//  Copyright © 2019 Dennis M. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var array = [Category]()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = array[indexPath.row] as Category
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data.title
+        cell.accessoryType = data.done ? .checkmark : .none
+        cell.backgroundColor = (cell.accessoryType == .checkmark) ? UIColor(rgb: 0xBDBDBD).withAlphaComponent(0.5) : UIColor.white
+        cell.textLabel?.textColor = (cell.accessoryType == .checkmark) ? UIColor.darkGray : UIColor.black
+        //        cell.layer.cornerRadius = 15.0
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+
+}
