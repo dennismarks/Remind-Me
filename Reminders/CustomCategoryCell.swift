99a7ea4e4abba571cd1d32a2e293cf0a75fa4336
dennismarks <dennismarkshome@gmail.com>
2019-05-27 22:18:14 -0400

    add views connection

---
+++ Reminders/CustomCategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCategoryCell.swift
+//  Reminders
+//
+//  Created by Dennis M on 2019-05-27.
+//  Copyright © 2019 Dennis M. All rights reserved.
+//
+
+import UIKit
+
+class CustomCategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
