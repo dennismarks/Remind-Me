ce3d5c2e23a9c7cf5b255ecffbad282ffea034d4
dennismarks <dennismarkshome@gmail.com>
2019-06-07 08:00:00 -0600

    add AddNewItemViewController

--- Reminders/Controller/AddNewItemViewController.swift
+++ Reminders/Controller/AddNewItemViewController.swift
@@ -8,28 +8,62 @@
 
 import UIKit
 
-class AddNewItemViewController: UIViewController {
+protocol AddNewItemDelegate {
+    func addNewItem(name: String)
+    func dismissView()
+}
 
-    @IBOutlet weak var textField: UITextField!
+class AddNewItemViewController: UIViewController {
+    
+    var active = false
+    var delegate: AddNewItemDelegate?
+    @IBOutlet weak var itemNameTextField: UITextField!
+    @IBOutlet weak var addReminderButton: UIButton!
+    @IBOutlet weak var datePickerView: UIDatePicker!
+    
+    
     
     override func viewDidLoad() {
-//        view.backgroundColor = UIColor.gr
-//        view.isOpaque = false
+        super.viewDidLoad()
     }
     
-//    @IBAction func donePressed(_ sender: Any) {
-//        print("Here")
-//
-//        if let presenter = presentingViewController as? ItemTableViewController {
-//            let item = Item(context: presenter.context)
-//            item.title = textField.text
-//            item.done = false
-//            item.parentCategory = presenter.selectedCategory
-//            presenter.array.append(item)
-//            presenter.save()
-//            presenter.tableView.reloadData()
-//            print("Saved")
-//        }
-//        self.dismiss(animated: true, completion: nil)
-//    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        self.delegate?.dismissView()
+    }
+    
+    @IBAction func dismissPressed(_ sender: UIButton) {
+        self.delegate?.dismissView()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addNewCategoryPressed(_ sender: UIButton) {
+        if let name = itemNameTextField.text {
+            self.delegate?.addNewItem(name: name)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addReminderPressed(_ sender: UIButton) {
+        if (active == false) {
+            print(active)
+            active = !active
+            self.preferredContentSize = CGSize(width: self.view.frame.width, height: self.view.frame.width)
+            UIView.animate(withDuration: 1) {
+                self.addReminderButton.setTitle("Dismiss", for: .normal)
+                self.datePickerView.layer.opacity = 1.0
+            }
+        } else {
+            active = !active
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(400), execute: {
+                self.preferredContentSize = CGSize(width: self.view.frame.width, height: 200)
+            })
+            UIView.animate(withDuration: 1) {
+                self.addReminderButton.setTitle("Add Reminder", for: .normal)
+                self.datePickerView.layer.opacity = 0.0
+            }
+        }
+        
+    }
+    
 }
