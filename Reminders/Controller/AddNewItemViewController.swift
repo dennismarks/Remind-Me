0d851ae463e78e59e3b6bffbd829db2cdd1230e1
dennismarks <dennismarkshome@gmail.com>
2019-06-20 09:10:00 -0600

    iphone se screen size add new item size opt

--- Reminders/Controller/AddNewItemViewController.swift
+++ Reminders/Controller/AddNewItemViewController.swift
@@ -21,6 +21,10 @@ class AddNewItemViewController: UIViewController {
     @IBOutlet weak var itemNameTextField: UITextField!
     @IBOutlet weak var addReminderButton: UIButton!
     @IBOutlet weak var datePickerView: UIDatePicker!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    var showArrow = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +33,23 @@ class AddNewItemViewController: UIViewController {
             self.itemNameTextField.becomeFirstResponder()
         })
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(260), execute: {
-            self.preferredContentSize = CGSize(width: self.view.frame.width, height: 200)
+            self.preferredContentSize = CGSize(width: self.view.frame.width, height: self.view.frame.width * 0.5)
 
         })
         
 //        self.preferredContentSize = CGSize(width: self.view.frame.width, height: 200)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        if self.view.frame.height < 340 {
+            stackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+            itemNameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 6).isActive = true
+//            buttonsStackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+//            buttonsStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -6).isActive = true
+        }
+//        if showArrow {
+//        }
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -78,20 +94,28 @@ class AddNewItemViewController: UIViewController {
         if (active == false) {
             print(active)
             active = !active
+//            hideArrow = true
+//            self.popoverPresentationController?.sourceView = self.view
             self.preferredContentSize = CGSize(width: self.view.frame.width, height: self.view.frame.width)
+//            self.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.maxY, width: 0, height: 0)
+            
             UIView.animate(withDuration: 1) {
+                
                 self.addReminderButton.setTitle("Dismiss", for: .normal)
                 self.datePickerView.layer.opacity = 1.0
             }
+//            self.viewWillAppear(true)
         } else {
             active = !active
-            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500), execute: {
-                self.preferredContentSize = CGSize(width: self.view.frame.width, height: 200)
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300), execute: {
+                self.preferredContentSize = CGSize(width: self.view.frame.width, height: self.view.frame.width * 0.5)
             })
+//            hideArrow = false
             UIView.animate(withDuration: 1) {
                 self.addReminderButton.setTitle("Add Reminder", for: .normal)
                 self.datePickerView.layer.opacity = 0.0
             }
+//            self.viewWillAppear(true)
         }
         
     }
